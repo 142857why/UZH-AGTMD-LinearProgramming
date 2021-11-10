b0098d91b40e44e76b23ef1d0f5c21e794907459
Rui Zhou <zhourui_1997@126.com>
2021-11-10 18:11:01 +0100

    Finish Task 2 a)

--- src/LPExercise2a.java
+++ src/LPExercise2a.java
@@ -3,7 +3,7 @@
 import edu.harvard.econcs.jopt.solver.IMIP;
 import edu.harvard.econcs.jopt.solver.IMIPResult;
 import edu.harvard.econcs.jopt.solver.client.SolverClient;
-import edu.harvard.econcs.jopt.solver.mip.MIP;
+import edu.harvard.econcs.jopt.solver.mip.*;
 
 public class LPExercise2a {
 
@@ -19,10 +19,34 @@ public class LPExercise2a {
 
 	private void buildLinearProgram() {
 		linearProgram = new MIP();
+		// TODO: Implement linear program.
+		Variable x1 = new Variable("x1", VarType.DOUBLE, -MIP.MAX_VALUE, MIP.MAX_VALUE);
+		Variable x2 = new Variable("x2", VarType.DOUBLE, -MIP.MAX_VALUE, MIP.MAX_VALUE);
 
-		/*
-		 * TODO: Implement linear program.
-		 */
+		linearProgram.add(x1);
+		linearProgram.add(x2);
+		linearProgram.setObjectiveMax(true);
+		linearProgram.addObjectiveTerm(0.63, x1);
+		linearProgram.addObjectiveTerm(0.37, x2);
+
+		Constraint c1 = new Constraint(CompareType.GEQ, 2);
+		c1.addTerm(1, x1);
+		c1.addTerm(-1, x2);
+		linearProgram.add(c1);
+
+		Constraint c2 = new Constraint(CompareType.EQ, 3);
+		c2.addTerm(1, x1);
+		c2.addTerm(0.25, x2);
+		linearProgram.add(c2);
+
+		Constraint c3 = new Constraint(CompareType.GEQ, 0);
+		c3.addTerm(1, x1);
+		linearProgram.add(c3);
+
+		Constraint c4 = new Constraint(CompareType.GEQ, 0);
+		c4.addTerm(1, x2);
+		linearProgram.add(c4);
+
 	}
 
 	public IMIPResult solve() {
